bdb7284f73d2c167dcbb5e15d4364203d90a5173
Amir <amir.benmallem@gmail.com>
2020-05-15 17:56:42 +0200

    shapes

---
+++ gl99/src/main/java/Amir/gl99/App.java
@@ -0,0 +1,13 @@
+package Amir.gl99;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
